f6cde124d21dc239558e9a3c51832d5403a2c30d
Sunayana <sunayana.agrawal@tredence.com>
2024-09-05 17:26:34 +0530

    initial commit

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Navbar from '../app/Navbar/navbar'
+import { Box } from '@mui/material'
+import DataTable from '..table/app/Component/table'
 
 export default function dashboard() {
   return (
-    <div>
-      You have logged in
-    </div>
+    <Box>
+      <Navbar />
+      <Box sx={{p: 2}}>
+      <Box sx={{p: 3, backgroundColor: "#fff" , boxShadow: 3 }}>
+      <DataTable />
+      </Box>
+      </Box>
+    </Box>
   )
 }
